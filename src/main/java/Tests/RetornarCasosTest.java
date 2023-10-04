49694c72b54a1b71dda6596cb11c11be96059240
William <william.debortoli@hotmail.com>
2023-10-04 19:29:08 -0300

    Retornar casos por sintoma feito.

---
+++ src/main/java/Tests/RetornarCasosTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tests;
+
+import Servidor.Database;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 2021122760291
+ */
+public class RetornarCasosTest {
+    public static void main(String[] args) {
+        Database database = new Database();
+        
+        ArrayList<String> sintomasPaciente1 = new ArrayList<>();
+        sintomasPaciente1.add("Dor");
+        sintomasPaciente1.add("Febre");
+        sintomasPaciente1.add("Tosse");
+        sintomasPaciente1.add("Fraqueza");
+        String diagnostico1 = "Gripe";
+        
+        ArrayList<String> sintomasPaciente2 = new ArrayList<>();
+        sintomasPaciente2.add("Tosse");
+        sintomasPaciente2.add("Fraqueza");
+        String diagnostico2 = "Asma";
+        
+        database.armazenarConsulta(sintomasPaciente1, diagnostico1);
+        database.armazenarConsulta(sintomasPaciente2, diagnostico2);
+        
+        System.out.println(database.retornarCasos());
+    }
+    
+}
