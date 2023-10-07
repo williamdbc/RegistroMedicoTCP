0f9bdd6b9a5aad37c96f0d6fda1772a8c1e2de1e
Priscila Candido <priscila.candido@vixteam.com.br>
2023-10-07 13:32:01 -0300

    Algoritmo Apriori

--- src/main/java/Servidor/algoritmoApriori/AlgoritmoApriori.java
+++ src/main/java/Servidor/algoritmoApriori/AlgoritmoApriori.java
@@ -1,238 +1,243 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Servidor.algoritmoApriori;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Vector;
-import java.util.stream.Collectors;
-
-
+/**
+ *
+ * @author prisc
+ */
+import java.util.*;
+import java.util.Map.Entry;
 public class AlgoritmoApriori {
-    
-    private double frequenciaMinima = 0.5;
-    private double confiancaMinima = 0.75;
-    private int numTransactions; 
-    private ArrayList<ArrayList<String>> sintomas;
-    private ArrayList<String> diagnosticos;
+    private static int nextSintomaId = 1;
+    private static double confiancaMinima = 0.75;
+    private static Map<String, Integer> sintomaToIntegerMap = new HashMap<>();
+    private static List<Set<String>> transactions;
 
-    public AlgoritmoApriori() {
-        sintomas = new ArrayList<ArrayList<String>>();
-       // diagnosticos = new Arrays.asList("1", "2", "3", "4"));
+    public static void main(String[] args) {
+        // Defina suas transações
+        List<Set<String>> transactions = new ArrayList<>();
+        transactions.add(new HashSet<>(Arrays.asList("Leite", "Pão", "Bolacha", "Suco")));
+        transactions.add(new HashSet<>(Arrays.asList("Leite", "Suco")));
+        transactions.add(new HashSet<>(Arrays.asList("Leite", "Ovos")));
+        transactions.add(new HashSet<>(Arrays.asList("Pão", "Bolacha", "Café")));
 
-        sintomas.add(new ArrayList<String>(Arrays.asList("Leite", "Pão", "Bolacha", "Suco")));
-        sintomas.add(new ArrayList<String>(Arrays.asList("Leite", "Pão", "Suco")));
-        sintomas.add(new ArrayList<String>(Arrays.asList("Leite", "Bolacha", "Suco")));
-        sintomas.add(new ArrayList<String>(Arrays.asList("Pão", "Bolacha", "Suco")));
+        double minSupport = 0.5; // Defina o suporte mínimo desejado
+        double confiancaMinima = 0.75; // Defina a confiança mínima desejada
+
+        // 1: Encontrar conjuntos de itens frequentes de tamanho 1 (itens frequentes individuais)
+        List<Set<String>> frequentItemsets1 = findFrequent1Itemsets(transactions, minSupport);
+
+        System.out.println("Frequent Itemsets of Size 1:");
+        printFrequentItemsets(frequentItemsets1);
+
+        int k = 2; // Comece com conjuntos de 2 itens
+        List<Set<String>> candidateItemsets = new ArrayList<>(frequentItemsets1);
         
-        numTransactions = sintomas.size();
-        String resultado = this.realizarDiagnostico(sintomas, diagnosticos);
+        List<Set<String>> frequentItemsetsK = null;
+        while (!candidateItemsets.isEmpty()) {
+            frequentItemsetsK = findFrequentKItemsets(transactions, candidateItemsets, minSupport);
+
+            // Agora você pode usar a lista frequentItemsetsK que contém todos os conjuntos frequentes de tamanho k
+            System.out.println("Frequent Itemsets of Size " + k + ":");
+            printFrequentItemsets(frequentItemsetsK);
+
+            candidateItemsets = generateCandidateItemsets(frequentItemsetsK, k);
+            k++;
+        }
         
+        
+        if (frequentItemsetsK != null) {
+            // 3: Calcular o suporte (frequência) de cada conjunto de itens frequente
+            Map<Set<String>, Integer> itemsetSupport = calculateSupport(frequentItemsetsK, transactions, minSupport);
+
+            System.out.println("Itemset Support:" + itemsetSupport.size());
+            printItemsetSupport(itemsetSupport, transactions.size());
+
+            // 4: Gerar regras de associação a partir dos conjuntos frequentes encontrados nas etapas anteriores
+            System.out.println("Association Rules:");
+            generateAssociationRules(frequentItemsetsK, itemsetSupport, confiancaMinima);
+        }
     }
     
-    //Primeira etapa:
-    //Gerar a Tabela de Candidatos com suas Frequências
-    ///Segunda etapa:
-    //Gerar tabela com itens frequentes
-    private ArrayList<String> calculateFrequentItemsets(ArrayList<ArrayList<String>> transactions, ArrayList<String> diagnosticos) {
-        ArrayList<String> frequentItemsets1 = new ArrayList<String>();
-        int frequencia = 0;
-        
-        for (ArrayList<String> transaction : transactions) {
-            for (String item : transaction) {
-                ArrayList<String> exists = frequentItemsets1.stream()
-                    .filter(x -> x.equals(item))
-                    .collect(Collectors.toCollection(ArrayList::new));
+    public static String realizarDiagnostico( ArrayList<ArrayList<String>> sintomas, ArrayList<String> diagnosticos){
+        return "teste";
+    }
+    
+    private static void printFrequentItemsets(List<Set<String>> itemsets) {
+        for (Set<String> itemset : itemsets) {
+            System.out.println(itemset);
+        }
+    }
 
-                if ( exists.isEmpty()) {
-                    frequentItemsets1.add(item);
+    private static void printItemsetSupport(Map<Set<String>, Integer> itemsetSupport, int totalTransactions) {
+        for (Entry<Set<String>, Integer> entry : itemsetSupport.entrySet()) {
+            double supportPercentage = (double) entry.getValue() / totalTransactions * 100;
+            System.out.println(entry.getKey() + " - Support: " + supportPercentage + "%");
+        }
+    }
+
+
+    private static List<Set<String>> findFrequent1Itemsets(List<Set<String>> transactions, double minSupport) {
+        Map<String, Integer> itemsetCount = new HashMap<>();
+        List<Set<String>> frequentItemsets = new ArrayList<>();
+
+        for (Set<String> transaction : transactions) {
+            for (String item : transaction) {
+                itemsetCount.put(item, itemsetCount.getOrDefault(item, 0) + 1);
+            }
+        }
+
+        // Criar conjuntos frequentes individuais na ordem em que aparecem nas transações
+        for (Set<String> transaction : transactions) {
+            for (String item : transaction) {
+                if (itemsetCount.get(item) >= minSupport * transactions.size()) {
+                    Set<String> itemset = new HashSet<>();
+                    itemset.add(item);
+                    if (!frequentItemsets.contains(itemset)) {
+                        frequentItemsets.add(itemset);
+                    }
                 }
             }
-         }
-        
-         for (String item : frequentItemsets1) {
-            System.out.print(item + " ");
         }
-       
-        
-        for (String item : frequentItemsets1) {
-            System.out.print(item + " ");
-        }
- 
-        System.out.print(frequentItemsets1.size());
 
-        return frequentItemsets1;
+        return frequentItemsets;
     }
 
-    public static List<Set<Integer>> generateKItemsets(List<Set<Integer>> frequentItemsets, int k) {
-        List<Set<Integer>> candidateItemsets = new ArrayList<>();
 
-        // Combine frequent (k-1)-itemsets to generate candidate k-itemsets
-        for (int i = 0; i < frequentItemsets.size(); i++) {
-            for (int j = i + 1; j < frequentItemsets.size(); j++) {
-                Set<Integer> itemset1 = frequentItemsets.get(i);
-                Set<Integer> itemset2 = frequentItemsets.get(j);
+    private static List<Set<String>> findFrequentKItemsets(List<Set<String>> transactions, List<Set<String>> candidateItemsets, double minSupport) {
+       Map<Set<String>, Integer> itemsetCount = new HashMap<>();
+        List<Set<String>> frequentItemsets = new ArrayList<>();
 
-                // Check if the first (k-1) items of the two itemsets are the same
-                List<Integer> itemList1 = new ArrayList<>(itemset1);
-                List<Integer> itemList2 = new ArrayList<>(itemset2);
+        for (Set<String> transaction : transactions) {
+            for (Set<String> candidate : candidateItemsets) {
+                boolean isFrequent = true;
+                for (String item : candidate) {
+                    if (!transaction.contains(item)) {
+                        isFrequent = false;
+                        break;
+                    }
+                }
+                if (isFrequent) {
+                    itemsetCount.put(candidate, itemsetCount.getOrDefault(candidate, 0) + 1);
+                }
+            }
+        }
+
+        for (Map.Entry<Set<String>, Integer> entry : itemsetCount.entrySet()) {
+            if (entry.getValue() >= minSupport * transactions.size()) {
+                frequentItemsets.add(entry.getKey());
+            }
+        }
+        
+        return frequentItemsets;
+    }
+
+    private static List<Set<String>> generateCandidateItemsets(List<Set<String>> frequentItemsetsKMinus1, int k) {
+       List<Set<String>> candidateItemsets = new ArrayList<>();
+
+        for (int i = 0; i < frequentItemsetsKMinus1.size(); i++) {
+            for (int j = i + 1; j < frequentItemsetsKMinus1.size(); j++) {
+                Set<String> itemset1 = frequentItemsetsKMinus1.get(i);
+                Set<String> itemset2 = frequentItemsetsKMinus1.get(j);
+
+                List<String> itemList1 = new ArrayList<>(itemset1);
+                List<String> itemList2 = new ArrayList<>(itemset2);
 
                 if (itemList1.subList(0, k - 2).equals(itemList2.subList(0, k - 2))) {
-                    Set<Integer> newCandidate = new HashSet<>(itemset1);
+                    Set<String> newCandidate = new HashSet<>(itemset1);
                     newCandidate.addAll(itemset2);
-
-                    // Check if all (k-1)-itemsets of the candidate are frequent
-                    if (isAllSubsetsFrequent(frequentItemsets, newCandidate, k - 1)) {
-                        candidateItemsets.add(newCandidate);
-                    }
+                    candidateItemsets.add(newCandidate);
                 }
             }
         }
 
         return candidateItemsets;
     }
-
-    private static boolean isAllSubsetsFrequent(List<Set<Integer>> frequentItemsets, Set<Integer> candidate, int k) {
-        List<Set<Integer>> subsets = generateSubsets(candidate, k);
-
-        for (Set<Integer> subset : subsets) {
-            if (!frequentItemsets.contains(subset)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static List<Set<Integer>> generateSubsets(Set<Integer> itemset, int k) {
-        List<Set<Integer>> subsets = new ArrayList<>();
-        Integer[] itemArray = itemset.toArray(new Integer[0]);
-        int n = itemArray.length;
-
-        generateSubsetsRecursive(subsets, itemArray, new HashSet<>(), 0, n, k);
-
+    
+    private static Set<Set<String>> generateSubsets(Set<String> itemset) {
+        Set<Set<String>> subsets = new HashSet<>();
+        generateSubsetsRecursive(itemset, new HashSet<>(), subsets);
         return subsets;
     }
-
-    private static void generateSubsetsRecursive(List<Set<Integer>> subsets, Integer[] itemArray, Set<Integer> currentSubset, int index, int n, int k) {
-        if (currentSubset.size() == k) {
-            subsets.add(new HashSet<>(currentSubset));
+    
+    private static void generateSubsetsRecursive(Set<String> remaining, Set<String> subset, Set<Set<String>> subsets) {
+        if (remaining.isEmpty()) {
+            if (!subset.isEmpty()) {
+                subsets.add(new HashSet<>(subset));
+            }
             return;
         }
 
-        if (index == n) {
-            return;
-        }
+        String element = remaining.iterator().next();
+        remaining.remove(element);
 
-        currentSubset.add(itemArray[index]);
-        generateSubsetsRecursive(subsets, itemArray, currentSubset, index + 1, n, k);
+        // Explore o ramo incluindo o elemento no subconjunto
+        subset.add(element);
+        generateSubsetsRecursive(remaining, subset, subsets);
 
-        currentSubset.remove(itemArray[index]);
-        generateSubsetsRecursive(subsets, itemArray, currentSubset, index + 1, n, k);
+        // Explore o ramo sem incluir o elemento no subconjunto
+        subset.remove(element);
+        generateSubsetsRecursive(remaining, subset, subsets);
+
+        remaining.add(element);
     }
 
-    
-    private String criarCandidato(ArrayList<ArrayList<String>> sintomas, String candidato ){
-        int frequencia = 0;
-        for (int i = 0; i < sintomas.size(); i++) {
-                ArrayList<String> sintomasPaciente = sintomas.get(i);
-                for(String sintoma: sintomasPaciente){
-                    if (candidato.equals(sintoma)) {
-                        frequencia += 1; 
-                        break;
-                    }
-                }
-                
-        }
-        
-        if(frequencia/numTransactions >= frequenciaMinima){
-            return candidato;
-        }
-        
-        return "";
-    }
-    
-    
-    private boolean candidatoIsInTabelaCandidatos(ArrayList<String> tabelaCandidatos, ArrayList<ArrayList<String>> sintomas, String candidato){
-        boolean candidatoExistente = false;
-        int i = 0;
-        for (String candidatoResult : tabelaCandidatos) {
-            ArrayList<String> result = sintomas.get(i);
-            for(String sintoma: result){
-                if (candidatoResult.equals(candidato)) {
-                    candidatoExistente = true;
-                    break;
+    private static void generateAssociationRules(List<Set<String>> frequentItemsets, Map<Set<String>, Integer> itemsetSupport, double minConfidence) {
+        for (Set<String> itemset : frequentItemsets) {
+            Set<Set<String>> itemSubsets = generateSubsets(itemset);
+
+            for (Set<String> subset : itemSubsets) {
+                Set<String> complement = new HashSet<>(itemset);
+                complement.removeAll(subset);
+
+                double confidence = calculateConfidence(subset, complement, itemsetSupport);
+
+                if (confidence >= minConfidence) {
+                    System.out.println(subset + " => " + complement + " (Confidence: " + confidence * 100 + "% )");
                 }
             }
-                
         }
-        
-        return candidatoExistente;
     }
-    
-    //Terceira etapa
-    //Quarta etapa
-    private ArrayList<String> gerarGrupoDeCandidatos(ArrayList<String> tabelaCandidatos, ArrayList<ArrayList<String>> sintomas, ArrayList<String> itensFrequentes){
-        int tam = sintomas.size();
 
-        HashSet<String> candidatosUnicos = new HashSet<>(); // Usamos um HashSet para manter candidatos únicos
-        ArrayList<String> candidatos = new ArrayList<String>();
+   private static double calculateConfidence(Set<String> antecedent, Set<String> consequent, Map<Set<String>, Integer> itemsetSupport) {
+        if (antecedent == null || consequent == null || !itemsetSupport.containsKey(antecedent)) {
+            return 0.0;
+        }
 
-        for(String candidatoX: tabelaCandidatos){
-            for(String candidatoY: tabelaCandidatos){
-                
-                if(!candidatoX.equals(candidatoY)){
-                    double frequencia = 0;
+        int antecedentSupport = itemsetSupport.get(antecedent);
 
-                    for (int i = 0; i < sintomas.size(); i++) {
-                        ArrayList<String> sintomasPaciente = sintomas.get(i);
-                        int cont = 0;
-                        for(String sintoma: sintomasPaciente){
+        Set<String> combined = new HashSet<>(antecedent);
+        combined.addAll(consequent);
 
-                            if(sintoma.equals(candidatoX) || sintoma.equals(candidatoY)){
-                                cont++;
-                            }
+        int combinedSupport = itemsetSupport.get(combined);
 
-                            if(cont == 2){
-                                frequencia += 1;
-                                break;
-                            }
-                        }
-                    }
+        return (double) combinedSupport / antecedentSupport;
+    }
 
-                    String candidatoCombination = candidatoX + ";" + candidatoY;
-                    // Verifica se já adicionou a combinação ou a combinação inversa
-                    if (!candidatosUnicos.contains(candidatoCombination) && !candidatosUnicos.contains(candidatoY+ ";" + candidatoX)) {
-                        if(frequencia/numTransactions >= frequenciaMinima){
-                            candidatosUnicos.add(candidatoCombination);  
-                        }
-                        
-                    }
+
+  private static Map<Set<String>, Integer> calculateSupport(List<Set<String>> frequentItemsetsK, List<Set<String>> transactions, double minSupport) {
+        Map<Set<String>, Integer> itemsetSupport = new HashMap<>();
+
+        int transactionCount = transactions.size();
+        int minSupportCount = (int) (minSupport * transactionCount);
+
+        for (Set<String> transaction : transactions) {
+            for (Set<String> itemset : frequentItemsetsK) {
+                if (transaction.containsAll(itemset)) {
+                    itemsetSupport.put(itemset, itemsetSupport.getOrDefault(itemset, 0) + 1);
                 }
             }
         }
 
-        return candidatos;
-    }
-    
-    //Quinta etapa
-    private ArrayList<Candidato> calcularConbiabilidade(ArrayList<Candidato> tabelaCandidatos){
-        return (ArrayList<Candidato>) tabelaCandidatos.stream().filter(x -> x.getFrequencia() >= this.frequenciaMinima);
-    }
-    
+        // Remover os itemsets que não atingem o suporte mínimo
+        itemsetSupport.entrySet().removeIf(entry -> entry.getValue() < minSupportCount);
 
-    
-  public String realizarDiagnostico(ArrayList<ArrayList<String>> sintomas, ArrayList<String> diagnostico) {
-        // 1  etapa e 2 etapa
-        ArrayList<String> tabelaCandidatos = calculateFrequentItemsets(sintomas, diagnostico);
+        return itemsetSupport;
+    }
 
-        return "Teste";
-    }
-    
-     public static void main(String[] args) {
-      
-        AlgoritmoApriori n = new AlgoritmoApriori();
-    }
+
 }
